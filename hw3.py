fe72d21240771c85093e556d7bff0f425856adbd
darkivist <kpaulj@protonmail.com>
2023-09-20 22:48:25 -0400

    updating hw3

--- hw3.py
+++ hw3.py
@@ -1,36 +1,38 @@
-#Paul Kelly
-#DATS 6312
-#Homework 3
-#9-25-23
+# Paul Kelly
+# DATS 6312
+# Homework 3
+# 9-25-23
 
-#E1
+# E1
 
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import wordnet
 
-#load moby.txt
+# load moby.txt
 f = open('moby.txt')
 raw = f.read()
 
-#tokenize text
+# tokenize text
 tokens = word_tokenize(raw)
 print("here we go!")
-#print total tokens
+# print total tokens
 print("total tokens:", (len(tokens)))
-#print unique tokens
-#could we use hapaxes here? ASK TA tomorrow
+# print unique tokens
+# could we use hapaxes here? ASK TA tomorrow
 print("total unique tokens:", (len(set(tokens))))
-#tag verb tokens with part of speech
+# tag verb tokens with part of speech
 wnl = nltk.WordNetLemmatizer()
 
-#source: https://www.geeksforgeeks.org/python-lemmatization-approaches-with-examples/
+
+# source: https://www.geeksforgeeks.org/python-lemmatization-approaches-with-examples/
 
 def pos_tagger(nltk_tag):
     if nltk_tag.startswith('V'):
         return wordnet.VERB
     else:
         return None
+
 
 tagged = nltk.pos_tag(tokens)
 wordnet_tagged = list(map(lambda x: (x[0], pos_tagger(x[1])), tagged))
@@ -49,23 +51,47 @@ lemmatized_corpus = " ".join(lemmatized_corpus)
 
 print("total unique tokens after verb lemmatization:", (len(set(lemmatized_corpus))))
 
-#lexical diversity
+# lexical diversity
 moby_diversity = float(len(set(raw))) / float(len(raw))
 print("The lexical diversity is: ", moby_diversity * 100, "%")
 
-#what percentage of corpus is "whale"?
+# what percentage of corpus is "whale"?
 
-print("Whale and whale are", (tokens.count("Whale")+tokens.count("whale"))/(len(tokens))*100, "% of tokens")
+print("Whale and whale are", (tokens.count("Whale") + tokens.count("whale")) / (len(tokens)) * 100, "% of tokens")
 
-#20 most frequently occurring tokens plus their frequency
+# 20 most frequently occurring tokens plus their frequency
 
 from nltk import FreqDist
+
 Freq_Dist = FreqDist(tokens)
 
 print("20 most frequently occurring tokens plus their frequencies are:", Freq_Dist.most_common(20))
+# tokens with length of greater than 6 and frequency of more than 160
+#source: https://stackoverflow.com/questions/45730590/filtering-tokens-from-a-list-by-multiple-conditions
+from collections import Counter
 
-#tokens with length of greater than 6 and frequency of more than 160
+c = Counter(tokens)
+selected_tokens = [x for x in set(tokens) if len(x) > 6 and c[x] > 160]
+print("Tokens with length of > 6 and frequency of > 160:",selected_tokens)
 
-#long_words = [words for words in tokens if len(words) > 6]
-#word_freq = nltk.FreqDist(long_words); print(word_freq.most_common(5))
-#print(list(nltk.bigrams(long_words)))
+# longest word in the text and its length
+
+#find length of longest token
+longest_len = max(len(s) for s in tokens); print(longest_len)
+print("length of longest token is:",longest_len,"characters")
+longest_token = [x for x in set(tokens) if len(x) >= longest_len]
+print("the longest word in the text is:", longest_token)
+
+#more_tokens = [x for x in set(tokens) if c[x] > 2000]
+#print(more_tokens)
+Freq_Dist_1 = FreqDist(x for x in set(tokens) if c[x] > 2000)
+
+print(c)
+#wordz = Freq_Dist_1.hapaxes() ; print(wordz)
+
+#Freq_Dist.hapaxes()
+#Once_happend= Freq_Dist.hapaxes() ; print(Once_happend)
+
+
+
+
