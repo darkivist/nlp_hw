b7401ec12a04a5667c6fbab34f2c705a40ff7719
darkivist <kpaulj@protonmail.com>
2023-12-04 12:06:44 -0500

    Updating exam 2 preparation code

--- exam2_prep/transformer_cnn_layer.py
+++ exam2_prep/transformer_cnn_layer.py
@@ -1,101 +1,132 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+
+# Load the IMDb dataset from the CSV file
+df = pd.read_csv('IMDB Dataset.csv')
+
+# Split data into training and testing sets
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+# Extract text and labels
+X_train, y_train = train_df['review'].values, (train_df['sentiment'] == 'positive').astype(int).values
+X_test, y_test = test_df['review'].values, (test_df['sentiment'] == 'positive').astype(int).values
+
+# Define a simple tokenizer function
+def simple_tokenizer(text):
+    return text.split()
+
+# Tokenize and prepare data
+max_length = 128
+
+# Ensure X_train and X_test are lists of strings
+X_train = list(X_train)
+X_test = list(X_test)
+
+# Tokenize inputs using the simple tokenizer
+X_train_tokenized = [simple_tokenizer(text)[:max_length] for text in X_train]
+X_test_tokenized = [simple_tokenizer(text)[:max_length] for text in X_test]
+
+# Convert text tokens to indices
+vocab = {word: idx + 1 for idx, word in enumerate(set(word for review in X_train_tokenized + X_test_tokenized for word in review))}
+vocab_size = len(vocab) + 1  # Add 1 for the padding token
+
+X_train_indices = [[vocab.get(word, 0) for word in review] for review in X_train_tokenized]
+X_test_indices = [[vocab.get(word, 0) for word in review] for review in X_test_tokenized]
+
+# Pad sequences to a fixed length
+X_train_padded = torch.nn.utils.rnn.pad_sequence([torch.tensor(review) for review in X_train_indices], batch_first=True)
+X_test_padded = torch.nn.utils.rnn.pad_sequence([torch.tensor(review) for review in X_test_indices], batch_first=True)
+
+# Create TensorDatasets
+train_dataset = TensorDataset(X_train_padded, torch.tensor(y_train))
+test_dataset = TensorDataset(X_test_padded, torch.tensor(y_test))
+
+train_loader = DataLoader(train_dataset, batch_size=16)
+test_loader = DataLoader(test_dataset, batch_size=16)
 
 
-# Custom Transformer model with CNN layer
-class TransformerWithCNN(nn.Module):
-    def __init__(self, input_dim, num_heads=8, ff_dim=512, num_blocks=4, num_filters=64, kernel_size=3, num_classes=10,
-                 dropout_rate=0.1):
-        super(TransformerWithCNN, self).__init__()
-
-        self.positional_enc = nn.Embedding(input_dim, input_dim)
-
-        # Define Transformer encoder blocks
-        encoder_layers = nn.TransformerEncoderLayer(d_model=input_dim, nhead=num_heads, dim_feedforward=ff_dim,
-                                                    dropout=dropout_rate)
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_blocks)
-
-        # CNN layer
-        self.cnn = nn.Conv1d(in_channels=input_dim, out_channels=num_filters, kernel_size=kernel_size,
-                             padding=(kernel_size - 1) // 2)
-
-        # Output layer
-        self.output_layer = nn.Linear(num_filters, num_classes)
+class TransformerCNN(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim, num_heads, hidden_dim, num_filters, filter_sizes, output_size, dropout):
+        super(TransformerCNN, self).__init__()
+        self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
+        self.transformer = torch.nn.TransformerEncoder(
+            torch.nn.TransformerEncoderLayer(
+                d_model=embedding_dim,
+                nhead=num_heads,
+                dim_feedforward=hidden_dim,
+                dropout=dropout
+            ),
+            num_layers=1
+        )
+        self.conv_layers = torch.nn.ModuleList([
+            torch.nn.Conv1d(in_channels=embedding_dim, out_channels=num_filters, kernel_size=fs)
+            for fs in filter_sizes
+        ])
+        self.fc = torch.nn.Linear(len(filter_sizes) * num_filters, output_size)
+        self.dropout = torch.nn.Dropout(dropout)
 
     def forward(self, x):
-        # Add positional encoding
-        positional_encoding = self.positional_enc(torch.arange(x.size(1)).unsqueeze(0).to(x.device))
-        x += positional_encoding
+        embedded = self.embedding(x)
+        embedded = embedded.permute(1, 0, 2)  # Change dimensions for transformer
+        transformer_output = self.transformer(embedded)
+        transformer_output = transformer_output.permute(1, 2, 0)  # Reshape for conv1d input
 
-        # Transformer encoding
-        x = self.transformer_encoder(x)
+        # Apply convolutions
+        conv_outputs = [conv(transformer_output) for conv in self.conv_layers]
+        pooled_outputs = [torch.nn.functional.max_pool1d(torch.nn.functional.relu(conv), conv.shape[2]).squeeze(2)
+                          for conv in conv_outputs]
+        cat = self.dropout(torch.cat(pooled_outputs, dim=1))
+        logits = self.fc(cat)
+        return logits
 
-        # Permute input for CNN layer (assuming the input is [batch_size, seq_len, input_dim])
-        x = x.permute(0, 2, 1)
+# Parameters for Transformer and CNN
+embedding_dim = 128
+num_heads = 4
+hidden_dim = 256
+num_filters = 100
+filter_sizes = [3, 4, 5]
+output_size = 2  # Positive and negative classes
+dropout = 0.5
 
-        # CNN layer
-        cnn_output = self.cnn(x)
-
-        # Global max pooling over sequence length
-        max_pool = torch.max(cnn_output, dim=2)[0]
-
-        # Output layer
-        output = self.output_layer(max_pool)
-        return output
-
-
-# Custom dataset for synthetic sequence data
-class SyntheticDataset(Dataset):
-    def __init__(self, num_samples, seq_length, input_dim):
-        self.data = torch.randn(num_samples, seq_length, input_dim)
-        self.labels = torch.randint(0, 2, (num_samples,), dtype=torch.long)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
-
-
-# Define constants
-input_dim = 64  # Dimensionality of input
-num_classes = 2
-num_samples = 1000
-seq_length = 20
-batch_size = 32
-num_epochs = 5
-
-# Create synthetic dataset
-dataset = SyntheticDataset(num_samples=num_samples, seq_length=seq_length, input_dim=input_dim)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-# Create an instance of the custom model
-model = TransformerWithCNN(input_dim=input_dim, num_classes=num_classes)
-
-# Define loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+# Instantiate the TransformerCNN model
+transformer_cnn_model = TransformerCNN(
+    vocab_size, embedding_dim, num_heads, hidden_dim, num_filters, filter_sizes, output_size, dropout
+)
 
 # Training loop
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    correct_predictions = 0
+optimizer = torch.optim.Adam(transformer_cnn_model.parameters(), lr=1e-3)
+criterion = torch.nn.CrossEntropyLoss()
 
-    for i, data in enumerate(dataloader, 0):
-        inputs, labels = data
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+transformer_cnn_model.to(device)
+
+epochs = 3
+for epoch in range(epochs):
+    transformer_cnn_model.train()
+    for inputs, labels in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}"):
+        inputs, labels = inputs.to(device), labels.to(device)
 
         optimizer.zero_grad()
-
-        outputs = model(inputs)
+        outputs = transformer_cnn_model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item()
-        _, predicted = torch.max(outputs, 1)
-        correct_predictions += (predicted == labels).sum().item()
+    # Evaluation
+    transformer_cnn_model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in tqdm(test_loader, desc=f"Evaluating - Epoch {epoch + 1}/{epochs}"):
+            inputs, labels = inputs.to(device), labels.to(device)
 
-    epoch_loss = running_loss / len(dataloader)
-    accuracy = correct_predictions / (len(dataloader) * batch_size)
-    print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {epoch_loss:.4f} - Accuracy: {accuracy * 100:.2f}%")
+            outputs = transformer_cnn_model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = correct / total
+    print(f"Epoch {epoch + 1}/{epochs} - Test Accuracy: {accuracy * 100:.2f}%")
--- exam2_prep/transformer_mlp_layer.py
+++ exam2_prep/transformer_mlp_layer.py
@@ -1,95 +1,115 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
 
+# Load the IMDb dataset from the CSV file
+df = pd.read_csv('IMDB Dataset.csv')
 
-# Custom Transformer model with MLP layer
-class TransformerWithMLP(nn.Module):
-    def __init__(self, input_dim, num_heads=8, ff_dim=512, num_blocks=4, mlp_hidden_dim=128, num_classes=10,
-                 dropout_rate=0.1):
-        super(TransformerWithMLP, self).__init__()
+# Split data into training and testing sets
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
-        self.positional_enc = nn.Embedding(input_dim, input_dim)
+# Extract text and labels
+X_train, y_train = train_df['review'].values, (train_df['sentiment'] == 'positive').astype(int).values
+X_test, y_test = test_df['review'].values, (test_df['sentiment'] == 'positive').astype(int).values
 
-        # Define Transformer encoder blocks
-        encoder_layers = nn.TransformerEncoderLayer(d_model=input_dim, nhead=num_heads, dim_feedforward=ff_dim,
-                                                    dropout=dropout_rate)
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_blocks)
+# Define a simple tokenizer function
+def simple_tokenizer(text):
+    return text.split()
 
-        # MLP layers
-        self.mlp = nn.Sequential(
-            nn.Linear(input_dim, mlp_hidden_dim),
-            nn.ReLU(),
-            nn.Linear(mlp_hidden_dim, num_classes)
+# Tokenize and prepare data
+max_length = 128
+
+# Ensure X_train and X_test are lists of strings
+X_train = list(X_train)
+X_test = list(X_test)
+
+# Tokenize inputs using the simple tokenizer
+X_train_tokenized = [simple_tokenizer(text)[:max_length] for text in X_train]
+X_test_tokenized = [simple_tokenizer(text)[:max_length] for text in X_test]
+
+# Convert text tokens to indices
+vocab = {word: idx + 1 for idx, word in enumerate(set(word for review in X_train_tokenized + X_test_tokenized for word in review))}
+vocab_size = len(vocab) + 1  # Add 1 for the padding token
+
+X_train_indices = [[vocab.get(word, 0) for word in review] for review in X_train_tokenized]
+X_test_indices = [[vocab.get(word, 0) for word in review] for review in X_test_tokenized]
+
+# Pad sequences to a fixed length
+X_train_padded = torch.nn.utils.rnn.pad_sequence([torch.tensor(review) for review in X_train_indices], batch_first=True)
+X_test_padded = torch.nn.utils.rnn.pad_sequence([torch.tensor(review) for review in X_test_indices], batch_first=True)
+
+# Create TensorDatasets
+train_dataset = TensorDataset(X_train_padded, torch.tensor(y_train))
+test_dataset = TensorDataset(X_test_padded, torch.tensor(y_test))
+
+train_loader = DataLoader(train_dataset, batch_size=16)
+test_loader = DataLoader(test_dataset, batch_size=16)
+
+class TransformerMLP(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim, transformer_dim, hidden_dim, num_layers, num_heads):
+        super(TransformerMLP, self).__init__()
+        self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
+        self.transformer = torch.nn.TransformerEncoder(
+            torch.nn.TransformerEncoderLayer(d_model=embedding_dim, nhead=num_heads),
+            num_layers=num_layers
+        )
+        self.fc = torch.nn.Sequential(
+            torch.nn.Linear(transformer_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, 1)  # Output size for binary classification
         )
 
     def forward(self, x):
-        # Add positional encoding
-        positional_encoding = self.positional_enc(torch.arange(x.size(1)).unsqueeze(0).to(x.device))
-        x += positional_encoding
+        embedded = self.embedding(x)
+        transformer_output = self.transformer(embedded)
+        transformer_output = transformer_output.mean(dim=1)  # Pooling the transformer output
+        output = self.fc(transformer_output)
+        return output.squeeze(1)
 
-        # Transformer encoding
-        x = self.transformer_encoder(x)
+# Parameters for Transformer and MLP
+vocab_size = vocab_size
+embedding_dim = 128
+transformer_dim = 128  # Should be the same as embedding_dim for this example
+hidden_dim = 256
+num_layers = 1
+num_heads = 4
 
-        # Global average pooling over sequence length
-        avg_pool = torch.mean(x, dim=1)
-
-        # MLP layers
-        output = self.mlp(avg_pool)
-        return output
-
-
-# Custom dataset for synthetic sequence data
-class SyntheticDataset(Dataset):
-    def __init__(self, num_samples, seq_length, input_dim):
-        self.data = torch.randn(num_samples, seq_length, input_dim)
-        self.labels = torch.randint(0, 2, (num_samples,), dtype=torch.long)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
-
-
-# Define constants
-input_dim = 64  # Dimensionality of input
-num_classes = 2
-num_samples = 1000
-seq_length = 20
-batch_size = 32
-num_epochs = 5
-
-# Create synthetic dataset
-dataset = SyntheticDataset(num_samples=num_samples, seq_length=seq_length, input_dim=input_dim)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-# Create an instance of the custom model
-model = TransformerWithMLP(input_dim=input_dim, num_classes=num_classes)
-
-# Define loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+# Instantiate the TransformerMLP model
+model = TransformerMLP(vocab_size, embedding_dim, transformer_dim, hidden_dim, num_layers, num_heads)
 
 # Training loop
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    correct_predictions = 0
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+criterion = torch.nn.BCEWithLogitsLoss()  # Binary cross-entropy loss for binary classification
 
-    for i, data in enumerate(dataloader, 0):
-        inputs, labels = data
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+epochs = 3
+for epoch in range(epochs):
+    model.train()
+    for inputs, labels in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}"):
+        inputs, labels = inputs.to(device), labels.to(device)
 
         optimizer.zero_grad()
-
         outputs = model(inputs)
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs, labels.float())  # Ensure labels are floats for BCEWithLogitsLoss
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item()
-        _, predicted = torch.max(outputs, 1)
-        correct_predictions += (predicted == labels).sum().item()
+    # Evaluation
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in tqdm(test_loader, desc=f"Evaluating - Epoch {epoch + 1}/{epochs}"):
+            inputs, labels = inputs.to(device), labels.to(device)
 
-    epoch_loss = running_loss / len(dataloader)
-    accuracy = correct_predictions / (len(dataloader) * batch_size)
-    print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {epoch_loss:.4f} - Accuracy: {accuracy * 100:.2f}%")
+            outputs = model(inputs)
+            predicted = torch.round(torch.sigmoid(outputs))  # Round the sigmoid output for binary predictions
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = correct / total
+    print(f"Epoch {epoch + 1}/{epochs} - Test Accuracy: {accuracy * 100:.2f}%")
